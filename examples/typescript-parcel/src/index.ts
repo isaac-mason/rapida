611e20a21e644c8a5e62326c5b8b5478fe5f5738
Isaac Mason <isaac@isaacmason.com>
2021-12-21 00:01:11 +1000

    Update parcel typescript example and add physics to the example

--- examples/typescript-parcel/src/index.ts
+++ examples/typescript-parcel/src/index.ts
@@ -1,4 +1,4 @@
-import { Component, Engine, Scene, World } from "@rapidajs/rapida";
+import { Component, Engine, Scene, World, Physics, BodyType } from "@rapidajs/rapida";
 import { AmbientLight, BoxGeometry, DirectionalLight, Mesh, MeshPhongMaterial, Vector3 } from "three";
 
 class SpinningCube extends Component {
@@ -6,23 +6,33 @@ class SpinningCube extends Component {
 
   scene: Scene;
 
-  constructor({ scene }: { scene: Scene }) {
+  physics: Physics;
+
+  constructor(params: { scene: Scene, physics: Physics }) {
     super();
-    this.scene = scene;
+    this.scene = params.scene;
+    this.physics = params.physics;
 
     const geometry = new BoxGeometry(10, 10, 10);
     const material = new MeshPhongMaterial();
     this.mesh = new Mesh(geometry, material);
+
+    this.physics.create.box({
+      size: [10, 10, 10],
+      mass: 1,
+      type: BodyType.DYNAMIC,
+      rotation: [0, 0, 0],
+      fixedRotation: false,
+      allowSleep: false,
+      angularVelocity: [1, 1, 1],
+      velocity: [0, 20, 0],
+
+    }, this.mesh);
   }
 
   onInit = () => {
     this.scene.add(this.mesh);
   }
-
-  onUpdate = (_t: number) => {
-    this.mesh.rotation.x += 0.01;
-    this.mesh.rotation.y += 0.01;
-  } 
 }
 
 const engine = new Engine();
@@ -30,7 +40,23 @@ const engine = new Engine();
 engine.run((ctx) => {
   const world = new World({ engine: ctx.engine });
 
-  const renderer = world.create.renderer.webgl({ domElementId: "rapida" });
+  const renderer = world.create.renderer.webgl();
+  document.getElementById('rapida').appendChild(renderer.domElement);
+
+  const physics = world.create.physics({
+    gravity: [0, -5, 0],
+  });
+
+  physics.create.plane({
+    type: BodyType.STATIC,
+    position: [0, -10, 0],
+    rotation: [-Math.PI / 2, 0, 0],
+    mass: 0,
+    material: {
+      friction: 0.0,
+      restitution: 0.3,
+    },
+  });
 
   const scene = world.create.scene();
 
@@ -52,7 +78,7 @@ engine.run((ctx) => {
 
   const space = world.create.space();
 
-  space.create.entity().addComponent(new SpinningCube({ scene }));
+  space.create.entity().addComponent(new SpinningCube({ scene, physics }));
 
   return world;
 });
